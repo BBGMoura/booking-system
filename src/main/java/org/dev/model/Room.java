14d5535b735255efac39470e05f93c586c1345cd
BBGMoura <brunamoura2000@hotmail.com>
2023-12-18 19:20:00 +0100

    initial room model

--- src/main/java/org/dev/model/Room.java
+++ src/main/java/org/dev/model/Room.java
@@ -1,12 +1,45 @@
 package org.dev.model;
 
-import jakarta.persistence.Column;
-import jakarta.persistence.GeneratedValue;
-import jakarta.persistence.GenerationType;
-import jakarta.persistence.Id;
+import jakarta.persistence.*;
 
 import java.math.BigDecimal;
 
+@Entity
 public class Room {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
 
+    private String name;
+
+    @Column(name = "price_per_half_hour")
+    private BigDecimal priceHalfHour;
+
+    @Column(name = "price_per_hour")
+    private BigDecimal priceHour;
+
+    public Room() {}
+
+    public Room(int id, String name, BigDecimal priceHalfHour, BigDecimal priceHour) {
+        this.id = id;
+        this.name = name;
+        this.priceHalfHour = priceHalfHour;
+        this.priceHour = priceHour;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public BigDecimal getPriceHalfHour() {
+        return priceHalfHour;
+    }
+
+    public BigDecimal getPriceHour() {
+        return priceHour;
+    }
 }
