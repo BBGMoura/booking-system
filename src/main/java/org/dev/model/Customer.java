99e81b36e806b1185c07599a0e5c80178e767c70
BBGMoura <brunamoura2000@hotmail.com>
2023-12-18 19:20:17 +0100

    initial Customer model

--- src/main/java/org/dev/model/Customer.java
+++ src/main/java/org/dev/model/Customer.java
@@ -1,4 +1,43 @@
 package org.dev.model;
 
+import jakarta.persistence.*;
+
+@Entity
 public class Customer {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "first_name")
+    private String first_name;
+
+    @Column(name = "last_name")
+    private String last_name;
+
+    private String email;
+
+    public Customer() {}
+
+    public Customer(int id, String first_name, String last_name, String email) {
+        this.id = id;
+        this.first_name = first_name;
+        this.last_name = last_name;
+        this.email = email;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public String getFirst_name() {
+        return first_name;
+    }
+
+    public String getLast_name() {
+        return last_name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
 }
