5f2c3ec5979c6534c7d52c0f0d2150889c0e1880
BBGMoura <brunamoura2000@hotmail.com>
2024-01-01 14:14:23 +0000

    Adding Booking History model

--- src/main/java/org/dev/model/BookingHistory.java
+++ src/main/java/org/dev/model/BookingHistory.java
@@ -1,4 +1,39 @@
 package org.dev.model;
 
+import jakarta.persistence.Column;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+
 public class BookingHistory {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private int id;
+
+    @Column(name = "booking_status")
+    private int bookingStatus;
+
+    @Column(name = "created_on")
+    private BookingStatus createdOn;
+
+    public BookingHistory() {
+    }
+
+    public BookingHistory(int id, int bookingStatus, BookingStatus createdOn) {
+        this.id = id;
+        this.bookingStatus = bookingStatus;
+        this.createdOn = createdOn;
+    }
+
+    public int getId() {
+        return id;
+    }
+
+    public int getBookingStatus() {
+        return bookingStatus;
+    }
+
+    public BookingStatus getCreatedOn() {
+        return createdOn;
+    }
 }
