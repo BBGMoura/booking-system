e76fe0eb79cb1ff534b158938456f242cf1be580
BBGMoura <brunamoura2000@hotmail.com>
2024-01-04 23:07:41 +0000

    test - add test class and save test

---
+++ src/test/java/org/dev/repository/CustomerRepositoryTest.java
@@ -0,0 +1,44 @@
+package org.dev.repository;
+
+import org.dev.model.Customer;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.jdbc.AutoConfigureTestDatabase;
+import org.springframework.boot.test.autoconfigure.orm.jpa.DataJpaTest;
+
+import org.junit.jupiter.api.Test;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static org.junit.jupiter.api.Assertions.assertEquals;
+
+
+@DataJpaTest
+@AutoConfigureTestDatabase(replace = AutoConfigureTestDatabase.Replace.NONE)
+class CustomerRepositoryTest {
+
+    @Autowired
+    private CustomerRepository customerRepository;
+
+    @Test
+    void testSave() {
+        //given
+        Customer customer = new Customer(1, "Lara", "Jean", "larajean@email.com");
+
+        //when
+        customerRepository.save(customer);
+
+        int customerId = customer.getId();
+        Customer retrievedCustomer = customerRepository.findById(customerId).orElseThrow();
+
+        List<Customer> retrievedCustomers = new ArrayList<Customer>();
+        customerRepository.findAll().forEach(retrievedCustomers::add);
+
+        //then
+        assertEquals(customerId, retrievedCustomer.getId());
+        assertEquals(customer.getEmail(), retrievedCustomer.getEmail());
+        assertEquals(customer.getFirstName(), retrievedCustomer.getFirstName());
+        assertEquals(customer.getLastName(), retrievedCustomer.getLastName());
+        assertEquals(1, retrievedCustomers.size());
+    }
+}
