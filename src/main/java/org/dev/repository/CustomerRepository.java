d13ae18d324e7bc6342a304b2f61ac1155e16b34
BBGMoura <brunamoura2000@hotmail.com>
2024-01-04 23:07:21 +0000

    feature - add query which finds customer by email

--- src/main/java/org/dev/repository/CustomerRepository.java
+++ src/main/java/org/dev/repository/CustomerRepository.java
@@ -4,6 +4,9 @@ import org.dev.model.Customer;
 import org.springframework.data.repository.CrudRepository;
 import org.springframework.stereotype.Repository;
 
+import java.util.Optional;
+
 @Repository
 public interface CustomerRepository extends CrudRepository<Customer, Integer> {
+    Optional<Customer> findByEmail(String email);
 }
