48e466587fc9198ddb66784c98ac6d1ebbeff8ac
BBGMoura <brunamoura2000@hotmail.com>
2024-01-10 20:42:19 +0000

    [F5] : Change CustomerService to include CustomerDTO

--- src/main/java/org/dev/serivce/CustomerService.java
+++ src/main/java/org/dev/serivce/CustomerService.java
@@ -1,16 +1,17 @@
 package org.dev.serivce;
 
+import org.dev.dto.CustomerDTO;
 import org.dev.model.Customer;
 
 import java.util.List;
 
 public interface CustomerService {
-    Customer createCustomer(Customer customer);
-    Customer getCustomerById(int customerId);
-    Customer getByEmail(String customerEmail);
-    List<Customer> getByFirstName(String firstName);
-    List<Customer> getByLastName(String lastName);
-    List<Customer> getAllCustomers();
-    Customer updateCustomer(Customer customer);
+    CustomerDTO createCustomer(CustomerDTO customerDto);
+    CustomerDTO getCustomerById(int customerId);
+    CustomerDTO getByEmail(String customerEmail);
+    List<CustomerDTO> getByFirstName(String firstName);
+    List<CustomerDTO> getByLastName(String lastName);
+    List<CustomerDTO> getAllCustomers();
+    CustomerDTO updateCustomer(CustomerDTO customerDto);
     void deleteCustomer(int customerId);
 }
