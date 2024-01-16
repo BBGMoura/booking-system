1e1d32c5efbc1184ca9c4b1b8abd19187f47b258
BBGMoura <brunamoura2000@hotmail.com>
2024-01-16 16:26:43 +0000

    [F5] : boilerplate code for customer repository.

--- src/main/java/org/dev/serivce/CustomerServiceImpl.java
+++ src/main/java/org/dev/serivce/CustomerServiceImpl.java
@@ -2,35 +2,40 @@ package org.dev.serivce;
 
 import org.dev.dto.CustomerDTO;
 import org.dev.repository.CustomerRepository;
+import org.springframework.stereotype.Service;
 
 import java.util.List;
 
+@Service
 public class CustomerServiceImpl implements CustomerService{
+    private final CustomerRepository customerRepository;
 
-    private CustomerRepository customerRepository;
+    public CustomerServiceImpl(final CustomerRepository customerRepository) {
+        this.customerRepository = customerRepository;
+    }
 
     @Override
-    public CustomerDTO createCustomer(CustomerDTO customerDto) {
+    public CustomerDTO createCustomer(final CustomerDTO customerDto) {
         return null;
     }
 
     @Override
-    public CustomerDTO getCustomerById(int customerId) {
+    public CustomerDTO getCustomerById(final int customerId) {
         return null;
     }
 
     @Override
-    public CustomerDTO getByEmail(String customerEmail) {
+    public CustomerDTO getByEmail(final String customerEmail) {
         return null;
     }
 
     @Override
-    public List<CustomerDTO> getByFirstName(String firstName) {
+    public List<CustomerDTO> getByFirstName(final String firstName) {
         return null;
     }
 
     @Override
-    public List<CustomerDTO> getByLastName(String lastName) {
+    public List<CustomerDTO> getByLastName(final String lastName) {
         return null;
     }
 
@@ -40,12 +45,12 @@ public class CustomerServiceImpl implements CustomerService{
     }
 
     @Override
-    public CustomerDTO updateCustomer(CustomerDTO customerDto) {
+    public CustomerDTO updateCustomer(final int customerId, final CustomerDTO customerDto) {
         return null;
     }
 
     @Override
     public void deleteCustomer(int customerId) {
-
+//
     }
 }
